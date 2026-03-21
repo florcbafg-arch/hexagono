e0b876185190e7c083a8cd6c29b7a8a10367719f
Flor Gonzalez <florcba.fg@gmail.com>
2026-03-20 21:15:04 -0300

    fix auth guard + validar rol + limpiar session corrupta

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,21 +1,28 @@
 // verificar sesión
+var usuario = JSON.parse(localStorage.getItem("hexagono_user"))
 
-const usuario = JSON.parse(localStorage.getItem("hexagono_user"))
+console.log("USUARIO STORAGE:", usuario)
 
+// si no hay usuario → login
 if(!usuario){
- window.location.href = "login.html"
+  window.location.href = "login.html"
+}
+
+// si no tiene rol → limpiar (caso bug)
+if(!usuario.rol){
+  localStorage.removeItem("hexagono_user")
+  window.location.href = "login.html"
 }
 
 // proteger rutas
-
 const pagina = window.location.pathname
 
 // admin no puede entrar a operario
 if(pagina.includes("operario") && usuario.rol !== "operario"){
- window.location.href = "admin.html"
+  window.location.href = "admin.html"
 }
 
 // operario no puede entrar a admin
 if(pagina.includes("admin") && usuario.rol !== "admin"){
- window.location.href = "operario.html"
+  window.location.href = "operario.html"
 }
