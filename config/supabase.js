bcdb884a80a1770f5a71f0f7ef73116f5eb3d723
Flor Gonzalez <florcba.fg@gmail.com>
2026-03-22 20:36:36 -0300

    debug env

--- config/supabase.js
+++ config/supabase.js
@@ -1,7 +1,12 @@
-const { createClient } = require('@supabase/supabase-js')
+console.log("ENV TEST:", process.env)
+
+const { createClient } = require("@supabase/supabase-js")
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+
+console.log("URL:", supabaseUrl)
+console.log("KEY:", supabaseKey)
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
