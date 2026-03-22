f7f021cb10b0d22bb9a9df688b49ede354b6e9da
Flor Gonzalez <florcba.fg@gmail.com>
2026-03-22 19:47:38 -0300

    fix(server): aumentar limite de request para excel

--- server/server.js
+++ server/server.js
@@ -21,7 +21,8 @@ app.use("/uploads", express.static(path.join(__dirname, "../uploads")))
 let registrosProduccion = []
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 
 app.use("/api/auth", authRoutes);
