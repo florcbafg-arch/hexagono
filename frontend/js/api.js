70bab4087651f233b38a9cd6ee3fcdc1f2cf1d91
Flor Gonzalez <florcba.fg@gmail.com>
2026-03-23 03:07:35 -0300

    fix: revert apiFetch para no romper programacion y mantener compatibilidad

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,7 +2,7 @@ function apiFetch(url, options = {}) {
 
   const token = localStorage.getItem("token")
 
-  return fetch("/api" + url, {
+  return fetch(url, {
     ...options,
     headers: {
       "Content-Type": "application/json",
